af8f60fb02d6f29b3c92b04d26da41c580e359e7
Kiranmayi M <kiranmayimadhavan07@gmail.com>
2026-09-18 20:27:45 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "roadsos-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./intel.html",
+  "./manifest.json",
+  "./roadsos-logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
